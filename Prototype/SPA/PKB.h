bdcc4caf652292f8bec1ee44a21af662204a2225
David Heryanto <david.heryanto@hotmail.com>
2013-10-16 03:33:19 +0800

    Forgot to include file

---
+++ Prototype/SPA/PKB.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include<stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+typedef short PROC;
+
+class PKB {
+public:
+};
---
+++ Prototype/SPA/Parser.cpp
@@ -0,0 +1,71 @@
+#include "Parser.h"
+
+using std::cout;
+
+Parser::Parser()
+{
+
+}
+
+Parser::~Parser()
+{
+
+}
+
+void Parser::parse(string filename)
+{
+
+}
+
+void Parser::parseProcedure()
+{
+
+
+}
+
+void Parser::parseStatementList()
+{
+
+}
+
+void Parser::parseStatement(string statement)
+{
+	cout << "Parsing statement: " << statement << endl;
+}
+
+// ...
+
+
+	////Skeleton for the parser class excluing the assisng, while, if ,call
+	//void assignFx(){
+	//	//Type your code here
+	//}
+	//void whileFx(){
+	//	//Type your code here
+	//}
+	//void ifFx(){
+	//	//Type your code here
+	//}
+	//void callFx(){
+	//	//Type your code here
+	//}
+
+	////retrieve the next string of stmt from the input - Melvin
+	//void getToken(){
+	//	//Type your code here
+	//}
+
+	//TNode getCurrentNode(){
+	//	//Type your code here
+	//}
+
+	//void parse(string filename) {
+	//	
+	//}
+
+	//void parseStatement(string statement)
+	//{
+	//	cout << "parsing statement: " << statement;
+	//}
+
+
