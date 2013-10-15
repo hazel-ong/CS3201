84be6c0bf66555632bea40cc04254a17bf2b4c27
melyong88 <kfyong@nus.edu.sg>
2013-10-15 14:14:49 +0800

    Added method headers into Parse class

--- Prototype/source/Parser.cpp
+++ Prototype/source/Parser.cpp
@@ -9,8 +9,29 @@ using namespace std;
 
 #include "PKB.h"
 #include "TNode.h"
+ 
+class Paser{
+public:
+	//Skeleton for the parser class excluing the assisng, while, if ,call
+	assignFx(){
+		//Type your code here
+	}
+	whileFx(){
+		//Type your code here
+	}
+	ifFx(){
+		//Type your code here
+	}
+	callFx(){
+		//Type your code here
+	}
+	
+	//retrieve the next string of stmt from the input - Melvin
+	getToken(){
+		//Type your code here
+	}
 
-int Parse () {
-
-	return 0;
-}
+	getCurrentNode(){
+		//Type your code here
+	}
+};
